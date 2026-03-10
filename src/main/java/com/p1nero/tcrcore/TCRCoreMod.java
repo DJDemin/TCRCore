133e501760ba846d769e60da8738850349ba7a35
p1nero <1070402601@qq.com>
2026-03-11 00:16:36 +0800

    修英文翻译key，加回中文补丁，修mimic，外置config防止开发环境影响

--- src/main/java/com/p1nero/tcrcore/TCRCoreMod.java
+++ src/main/java/com/p1nero/tcrcore/TCRCoreMod.java
@@ -154,6 +154,13 @@ public class TCRCoreMod {
             event.addRepositorySource((packConsumer) -> packConsumer.accept(pack));
         }
         if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+            String name = "zh_cn_patch";
+            var resourcePath = ModList.get().getModFileById(MOD_ID).getFile().findResource("packs/" + name);
+            var pack = Pack.readMetaAndCreate(name, Component.literal("Chinese Patch"), true,
+                    (path) -> new PathPackResources(path, resourcePath, false), PackType.CLIENT_RESOURCES, Pack.Position.TOP, PackSource.BUILT_IN);
+            event.addRepositorySource((packConsumer) -> packConsumer.accept(pack));
+        }
+        if (event.getPackType() == PackType.CLIENT_RESOURCES) {
             String name = "tcr_assets";
             var resourcePath = ModList.get().getModFileById(MOD_ID).getFile().findResource("packs/" + name);
             var pack = Pack.readMetaAndCreate(name, Component.literal("The Casket of Reveries Assets - Override"), true,
